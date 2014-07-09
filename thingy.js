b90b6625b9c370e7c6591a92341f71dd1a1fbc7f
Ryan Hughes <ryan@iheartryan.com>
2014-07-09 07:35:03 -0400

    A little refactoring.

--- thingy.js
+++ thingy.js
@@ -3,89 +3,111 @@ $(document).ready(function() {
   var dc;
 
   $('button#offerbtn').click(function () {
-    pc = new RTCPeerConnection(null);
-    dc = pc.createDataChannel('zapchan');
-    pc.createOffer(
-      function (desc) {
-        pc.setLocalDescription(
-          desc,
-          function () {
-            console.log(desc);
-            $('#offertxt').append(JSON.stringify(desc));
-          },
-          function (e) { console.log(e); }
-        );
-      },
-      function (e) { console.log(e); }
-    );
+    var retval = createOffer();
+    pc = retval.peerConnection;
+    dc = retval.dataChannel;
   });
 
   $('button#answerbtn').click(function () {
     var offertxt = $('#offertxt').val();
-    var desc;
-    try {
-      desc = JSON.parse(offertxt);
-    } catch(e) {
-      console.log(e);
-      return;
-    }
-    pc = new RTCPeerConnection(null);
-    pc.ondatachannel = function (event) {
-      dc = event.channel;
-
-      dc.onopen = dataChannelOpen;
-
-      dc.onmessage = function (event) {
-        console.log("Data: ", event.data);
-      };
-    };
-
-    pc.setRemoteDescription(
-      new RTCSessionDescription(desc),
-      function () {
-        pc.createAnswer(
-          function (answer) {
-            pc.setLocalDescription(
-              new RTCSessionDescription(answer),
-              function () {
-                console.log(answer);
-                var anstxt = JSON.stringify(answer);
-                $('#answertxt').append(anstxt);
-              }
-            );
-          },
-          function (e) { console.log(e); }
-        );
-      },
-      function (e) { console.log(e); }
-    );
+    createAnswer(offertxt);
   });
 
   $('#recvansbtn').click(function () {
     var anstxt = $('#answertxt').val();
-    console.log(anstxt);
-    try {
-      desc = JSON.parse(anstxt);
-    } catch(e) {
-      console.log(e);
-      return;
-    }
-
-    dc.onmessage = function (event) {
-      console.log("Data: ", event.data);
-    };
-
-    dc.onopen = dataChannelOpen;
-
-    pc.setRemoteDescription(
-      new RTCSessionDescription(desc),
-      function () {
-        console.log("Session established");
-      },
-      function (e) { console.log(e); }
-    );
+    recvAnswer(anstxt, pc);
   });
 });
+
+function createOffer() {
+  var pc = new RTCPeerConnection(null);
+  var dc = pc.createDataChannel('zapchan');
+  dc.onopen = dataChannelOpen;
+  var offertxt;
+  pc.createOffer(
+    function (offer) {
+      pc.setLocalDescription(
+        offer,
+        function () {
+          console.log(offer);
+          publishOffer(offer);
+        },
+        function (e) { console.log(e); }
+      );
+    },
+    function (e) { console.log(e); }
+  );
+
+  return {
+    peerConnection: pc,
+    dataChannel: dc,
+  };
+}
+
+function createAnswer(offertxt) {
+  var desc;
+  try {
+    desc = JSON.parse(offertxt);
+  } catch(e) {
+    console.log(e);
+    return;
+  }
+  var pc = new RTCPeerConnection(null);
+  pc.ondatachannel = function (event) {
+    dc = event.channel;
+
+    dc.onopen = dataChannelOpen;
+  };
+
+  pc.setRemoteDescription(
+    new RTCSessionDescription(desc),
+    function () {
+      pc.createAnswer(
+        function (answer) {
+          pc.setLocalDescription(
+            new RTCSessionDescription(answer),
+            function () {
+              publishAnswer(answer);
+            }
+          );
+        },
+        function (e) { console.log(e); }
+      );
+    },
+    function (e) { console.log(e); }
+  );
+}
+
+function recvAnswer(anstxt, pc) {
+  console.log(anstxt);
+  var desc;
+  try {
+    desc = JSON.parse(anstxt);
+  } catch(e) {
+    console.log(e);
+    return;
+  }
+
+  // The dataChannel's onopen handler was already set by createOffer.
+
+  pc.setRemoteDescription(
+    new RTCSessionDescription(desc),
+    function () {
+      console.log("Session established");
+    },
+    function (e) { console.log(e); }
+  );
+}
+
+function publishOffer(offer) {
+  $('#offertxt').append(JSON.stringify(offer));
+}
+
+function publishAnswer(answer) {
+  console.log(answer);
+  var anstxt = JSON.stringify(answer);
+  $('#answertxt').append(anstxt);
+}
 
 function dataChannelOpen() {
   var dc = this;
