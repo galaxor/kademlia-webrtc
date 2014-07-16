5f4dd31b9242150da5212c7d8be468c9efdaeda0
Ryan Hughes <ryan@iheartryan.com>
2014-07-16 07:56:36 -0400

    Wrap the WebRTC stuff a bit.

---
+++ WebRTCPeer.js
@@ -0,0 +1,110 @@
+function WebRTCPeer(namespace) {
+  this.pc = null;
+  this.dc = null;
+  this.msgHandlers = [];
+
+  if (typeof namespace == "undefined") {
+    this.RTCPeerConnection = RTCPeerConnection;
+    this.RTCSessionDescription = RTCSessionDescription;
+  } else {
+    this.RTCPeerConnection = namespace.RTCPeerConnection;
+    this.RTCSessionDescription = namespace.RTCSessionDescription;
+  }
+}
+
+WebRTCPeer.prototype.createOffer = function (callback) {
+  var peer = this;
+
+  this.pc = new this.RTCPeerConnection(null);
+  this.dc = this.pc.createDataChannel('zapchan');
+  this.dc.onopen = function () { peer.dataChannelOpen() };
+  var offertxt;
+  this.pc.createOffer(
+    function (offer) {
+      peer.pc.setLocalDescription(
+        offer,
+        function () {
+          callback(offer);
+        },
+        function (e) { console.log(e); }
+      );
+    },
+    function (e) { console.log(e); }
+  );
+
+  return {
+    peerConnection: this.pc,
+    dataChannel: this.dc,
+  };
+};
+
+WebRTCPeer.prototype.createAnswer = function (offertxt, callback) {
+  var desc;
+  try {
+    desc = JSON.parse(offertxt);
+  } catch(e) {
+    console.log(e);
+    return;
+  }
+  this.pc = new this.RTCPeerConnection(null);
+  pc.ondatachannel = function (event) {
+    peer.dc = event.channel;
+
+    peer.dc.onopen = function () { peer.dataChannelOpen(this); };
+  };
+
+  this.pc.setRemoteDescription(
+    new this.RTCSessionDescription(desc),
+    function () {
+      peer.pc.createAnswer(
+        function (answer) {
+          peer.pc.setLocalDescription(
+            new this.RTCSessionDescription(answer),
+            function () {
+              callback(answer);
+            }
+          );
+        },
+        function (e) { console.log(e); }
+      );
+    },
+    function (e) { console.log(e); }
+  );
+};
+
+WebRTCPeer.prototype.recvAnswer(anstxt) {
+  console.log(anstxt);
+  var desc;
+  var peer = this;
+  try {
+    desc = JSON.parse(anstxt);
+  } catch(e) {
+    console.log(e);
+    return;
+  }
+
+  // The dataChannel's onopen handler was already set by createOffer.
+
+  this.pc.setRemoteDescription(
+    new this.RTCSessionDescription(desc),
+    function () {
+      console.log("Session established");
+    },
+    function (e) { console.log(e); }
+  );
+};
+
+WebRTCPeer.prototype.dataChannelOpen = function (dc) {
+  var peer = this;
+  dc.onmessage = function (event) { peer.onMessage(event); };
+};
+
+WebRTCPeer.prototype.addMsgHandler = function (handler) {
+  this.msgHandlers[this.msgHandlers.length] = handler;
+};
+
+WebRTCPeer.prototype.onMessage = function (event) {
+  for (var i=0; i<this.msgHandlers.length; i++) {
+    this.msgHandlers[i](event);
+  }
+};
