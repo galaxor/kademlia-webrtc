74619331483d644de4acd02a0e9ddacc38eb5ee6
Ryan Hughes <ryan@iheartryan.com>
2014-10-31 11:21:15 -0400

    Peer side uses new unified WebRTCPeer library.

--- peer.js
+++ peer.js
@@ -1,222 +1,3 @@
-function WebRTCPeer () {
-  this.pendingDataChannels = {};
-  this.dataChannels = {};
-  this.pendingIceCandidates = [];
-  this.pc = null;
-
-  // We expect you to have included ../dist/wrtc.js via a <script> tag before
-  // including this one.  That will give us the definition of wrtc.
-  this.RTCPeerConnection = wrtc.RTCPeerConnection;
-  this.RTCSessionDescription = wrtc.RTCSessionDescription;
-  this.RTCIceCandidate = wrtc.RTCIceCandidate;
-
-  this.localOrRemoteDescSet = false;
-
-  this.dataChannelSettings = {
-    'reliable': {
-          outOfOrderAllowed: false,
-          maxRetransmitNum: 10
-        },
-  };
-
-  this.iceXferReadyCallbacks = [];
-  this.sendLocalIceCandidateHandlers = [];
-  this.sendOfferHandlers = [];
-  this.dataChannelsOpenCallbacks = [];
-
-  this.inboundIceCandidates = [];
-}
-
-// in bridge.js
-WebRTCPeer.prototype._iceXferReady = function () {
-  var ready = null;
-  this.iceXferReadyCallbacks.every(function (cb) {
-    ready = cb();
-    return ready;
-  });
-  return ready;
-};
-
-// in bridge.js
-WebRTCPeer.prototype.addSendLocalIceCandidateHandler = function (handler) {
-  this.sendLocalIceCandidateHandlers.push(handler);
-};
-
-// in bridge.js
-WebRTCPeer.prototype.addIceXferReadyCallback = function (cb) {
-  this.iceXferReadyCallbacks.push(cb);
-};
-
-WebRTCPeer.prototype.addSendOfferHandler = function (handler) {
-  this.sendOfferHandlers.push(handler);
-};
-
-// in bridge.js
-WebRTCPeer.prototype.addDataChannelsOpenCallback = function (cb) {
-  this.dataChannelsOpenCallbacks.push(cb);
-};
-
-WebRTCPeer.prototype._doHandleError = function (error) {
-  throw error;
-};
-
-WebRTCPeer.prototype._doAllDataChannelsOpen = function () {
-  var peer = this;
-  console.log('complete');
-  this.dataChannelsOpenCallbacks.forEach(function (cb) {
-    cb(peer.dataChannels);
-  });
-};
-
-WebRTCPeer.prototype._doWaitforDataChannels = function () {
-  console.log('awaiting data channels');
-};
-
-WebRTCPeer.prototype.createOffer = function () {
-  var peer = this;
-  this.pc = new this.RTCPeerConnection(
-    {
-      iceServers: [{url:'stun:stun.l.google.com:19302'}]
-    },
-    {
-      'optional': []
-    }
-  );
-  this.pc.onsignalingstatechange = function(event) {
-    console.info('signaling state change:', event);
-  };
-  this.pc.oniceconnectionstatechange = function(event) {
-    console.info("ice connection state change: ", event.target.iceConnectionState);
-  };
-  this.pc.onicegatheringstatechange = function(event) {
-    console.info("ice gathering state change: ", event.target.iceGatheringState);
-  };
-  this.pc.onicecandidate = function(event) {
-    var candidate = event.candidate;
-    if(!candidate) return;
-    if (peer._iceXferReady()) {
-      peer._xferIceCandidate(candidate);
-    } else {
-      peer.pendingIceCandidates.push(candidate);
-    }
-  };
-
-  this._doCreateDataChannels();
-  this._doCreateOffer();
-};
-
-WebRTCPeer.prototype._doCreateDataChannels = function () {
-  var peer = this;
-  var labels = Object.keys(this.dataChannelSettings);
-  labels.forEach(function(label) {
-    var channelOptions = peer.dataChannelSettings[label];
-    peer.createDataChannel(label, channelOptions);
-  });
-};
-
-WebRTCPeer.prototype.createDataChannel = function (label, channelOptions) {
-  var peer = this;
-  var channel = peer.pendingDataChannels[label] = peer.pc.createDataChannel(label, channelOptions);
-  var labels = Object.keys(this.dataChannelSettings);
-  console.log(channel);
-  channel.binaryType = 'arraybuffer';
-  channel.onopen = function() {
-    console.info('onopen', label);
-    peer.dataChannels[label] = channel;
-    delete peer.pendingDataChannels[label];
-    if(Object.keys(peer.dataChannels).length === labels.length) {
-      peer._doAllDataChannelsOpen();
-    }
-  };
-  channel.onmessage = function(event) {
-    var data = event.data;
-    if('string' == typeof data) {
-      console.log('onmessage:', data);
-    } else {
-      console.log('onmessage:', new Uint8Array(data));
-    }
-  };
-  channel.onclose = function(event) {
-    console.info('onclose');
-  };
-  channel.onerror = peer._doHandleError.bind(peer);
-};
-
-WebRTCPeer.prototype._doCreateOffer = function () {
-  var peer = this;
-  this.pc.createOffer(
-    function (offer) {
-      peer.pc.setLocalDescription(
-        new peer.RTCSessionDescription(offer),
-        peer._doSendOffer.bind(peer, offer),
-        peer._doHandleError.bind(peer)
-      );
-    },
-    this._doHandleError.bind(peer)
-  );
-};
-
-WebRTCPeer.prototype._doSendOffer = function (offer) {
-  this.localOrRemoteDescSet = true;
-  this.inboundIceCandidates.forEach(function(candidate) {
-    peer.pc.addIceCandidate(new peer.RTCIceCandidate(candidate.sdp));
-  });
-
-  var offerObj = {
-    'type': offer.type,
-    'sdp': offer.sdp,
-  };
-
-  this.sendOfferHandlers.forEach(function (handler) {
-    handler(offerObj);
-  });
-};
-
-
-// in bridge.js
-WebRTCPeer.prototype._xferIceCandidate = function (candidate) {
-  var iceCandidate = {
-    'type': 'ice',
-    'sdp': {
-      'candidate': candidate.candidate,
-      'sdpMid': candidate.sdpMid,
-      'sdpMLineIndex': candidate.sdpMLineIndex
-    }
-  };
-
-  console.info(JSON.stringify(iceCandidate));
-
-  this.sendLocalIceCandidateHandlers.forEach(function (handler) {
-    handler(iceCandidate);
-  });
-};
-
-WebRTCPeer.prototype.recvAnswer = function (desc) {
-  var peer = this;
-  this.pc.setRemoteDescription(
-    new peer.RTCSessionDescription(desc),
-    peer._doWaitforDataChannels.bind(peer),
-    peer._doHandleError.bind(peer)
-  );
-};
-
-// in bridge.js
-WebRTCPeer.prototype.sendPendingIceCandidates = function () {
-  var peer = this;
-  this.pendingIceCandidates.forEach(function(candidate) {
-    peer._xferIceCandidate(candidate);
-  });
-};
-
-// in bridge.js
-WebRTCPeer.prototype.recvRemoteIceCandidate = function (data) {
-  if (this.localOrRemoteDescSet) {
-    this.pc.addIceCandidate(new this.RTCIceCandidate(data.sdp.candidate));
-  } else {
-    this.inboundIceCandidates.push(data);
-  }
-};
-
 (function() {
 
 var host = window.location.host.split(':')[0];
@@ -225,7 +6,26 @@ var bridge = window.location.toString().split('?')[1] || host + ':9001';
 var ws = null;
 ws = new WebSocket("ws://" + bridge);
 
-var peer = new WebRTCPeer(wrtc);
+var peer = new WebRTCPeer({
+  createDataChannels: {
+    reliable: {
+      outOfOrderAllowed: false,
+      maxRetransmitNum: 10,
+      onOpen: function (channel) {
+        var data = new Uint8Array([97, 99, 107, 0]);
+        channel.send(data.buffer);
+        channel.send("Hello bridge!");
+      },
+      onMessage: function (channel, data) {
+        if('string' == typeof data) {
+          console.log('onmessage:',channel.label, data);
+        } else {
+          console.log('onmessage:',channel.label, new Uint8Array(data));
+        }
+      },
+    },
+  },
+});
 
 peer.addSendLocalIceCandidateHandler(function (candidate) {
   ws.send(JSON.stringify(candidate));
@@ -233,15 +33,8 @@ peer.addSendLocalIceCandidateHandler(function (candidate) {
 peer.addIceXferReadyCallback(function (cb) {
   return WebSocket.OPEN == ws.readyState;
 });
-peer.addDataChannelsOpenCallback(function (dataChannels) {
-  var data = new Uint8Array([97, 99, 107, 0]);
-  dataChannels['reliable'].send(data.buffer);
-  dataChannels['reliable'].send("Hello bridge!");
-});
 
 ws.onopen = function() {
-  peer.sendPendingIceCandidates();
-
   peer.addSendOfferHandler(function (offer) {
     ws.send(JSON.stringify(offer));
   });
