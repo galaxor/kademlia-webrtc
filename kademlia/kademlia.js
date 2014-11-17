0c9286b5db5958bb1a84c1a81f450435d19b94b6
Ryan Hughes <ryan@iheartryan.com>
2014-11-16 23:10:15 -0500

    Starting to work on a kademlia implementation.

---
+++ kademlia/kademlia.js
@@ -0,0 +1,106 @@
+var crypt = require('crypto-lite').crypto;
+var bitCoder = require('bit-coder');
+
+/**
+ * Create a distributed hash table using the Kademlia protocol as specified here:
+ *    http://xlattice.sourceforge.net/components/protocol/kademlia/specs.html
+ *
+ * @param options An object with some of these keys.  If a default exists, it is listed here.
+ *    alpha - integer (default: 3): The amount of concurrent RPCs allowed.
+ *            (we actually use "loose parallelism" - we send the next iteration
+ *            of the RPC algorithm after a timeout has passed, rather than
+ *            waiting for all the RPCs to return)
+ *    B - integer (default: 256): The number of bits in the keys used to
+ *        identify nodes and values.  For these keys, we use SHA256 hashes, hence 256 bits.
+ *    k - integer (default: 20):  The maximum number of contacts stored in a bucket.
+ *    tExpire - integer in seconds (default: 86410 [~one day]): The time after the
+ *              original publication date after which a key/value pair expires.
+ *    tRepublish - integer in seconds (default: 86400 [1 day]): After this
+ *                 time, the original publisher must republish a key/value pair.
+ *    tRefresh - integer in seconds (default: 3600 [1hr]): If a bucket has not
+ *               been accessed in this amount of time, refresh it.
+ *    tReplicate - integer in seconds (default: 3600 [1hr]): After this amount
+ *                 of time, a node must publish its entire database.
+ */
+function KademliaDHT(options) {
+  var characteristics = {
+    alpha: 3,
+    B: 256,
+    k: 20,
+    tExpire: 86410,
+    tRepublish: 86400,
+    tRefresh: 3600,
+    tReplicate: 3600,
+    id: null,
+  };
+
+  // Set the defaults.
+  for (k in characteristics) {
+    this[k] = characteristics[k];
+  }
+
+  if (typeof options == 'string') {
+    this.id = options;
+  } else {
+    // Allow the defaults to be overridden.
+    for (k in characteristics) {
+      if (typeof options[k] != "undefined") {
+        this[k] = options[k];
+      }
+    }
+  }
+
+  // Initialize the buckets.
+  this.buckets = new Array(this.B);
+  for (var i=0; i<this.buckets.length; i++) {
+    this.buckets[i] = new Array(this.k);
+  }
+}
+
+module.exports = exports = KademliaDHT;
+
+KademliaDHT.prototype._hex2BitStream = function (hex) {
+  // If the number of bits is not a multiple of 32, I might have to pad the
+  // data, and I don't feel like it.
+  if (hex.length % 8) {
+    throw new Error("The size of the hex is not a multiple of 32 bits.");
+  }
+
+  // Allocate a buffer.  1 hex char == 4 bits, so the size of the buffer is 4x
+  // the length of the string.
+  var buf = new bitCoder.BitStream(hex.length*4);
+
+  // 32-bit chunks.
+  for (var i=0; i<hex.length; i+=8) {
+    var chunk = parseInt(hex.substr(i, 8), 16);
+    buf.writeBits(chunk, 32);
+  }
+
+  buf.index = 0;
+  return buf;
+}
+
+KademliaDHT.prototype._bitCmp = function (b1, b2) {
+  // If we were asked to compare bitstreams of unequal length, we'd have to pad
+  // the shorter one, and I don't feel like it.
+  if (b1.length != b2.length) {
+    throw new Error("The operands are not the same size.");
+  }
+
+  for (b1.index=0, b2.index=0; b1.index < b1.view.length; ) {
+    var chunk1 = b1.readBits(32);
+    var chunk2 = b2.readBits(32);
+    if (chunk1 < chunk2) {
+      return 1;
+    } else if (chunk1 > chunk2) {
+      return -1;
+    }
+  }
+  return 0;
+}
+
+// var dht = new KademliaDHT('1234');
+// var b1 = dht._hex2BitStream('1234567812345678');
+// var b2 = dht._hex2BitStream('1234567812345678');
+// 
+// console.log(dht._bitCmp(b1, b2));
