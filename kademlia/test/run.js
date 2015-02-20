c60cdb0ee0000a7e1db65cf34fb7f78e163335fa
Ryan Hughes <ryan@iheartryan.com>
2015-02-20 09:01:54 -0500

    Test of using nearby buckets.

--- kademlia/test/run.js
+++ kademlia/test/run.js
@@ -231,7 +231,49 @@ describe("KademliaDHT", function () {
     });
 
     it("should return nodes from nearby buckets if the target bucket doesn't have enough nodes, but nearby buckets have some.", function () {
-      assert(0);
+      var kademlia = mockTimedKademlia();
+
+      var dht = new kademlia.KademliaDHT({B: 32, id: '00000000'});
+
+      var keys = [
+        '08000001',
+        '10000001',
+        '20000001',
+        '40000001',
+        '80000001',
+      ];
+      for (var i=0; i<keys.length; i++) {
+        var key1 = keys[i];
+        var b1   = dht._hex2BitStream(key1);
+        var willRespondNode = new kademlia.KademliaRemoteNode({id: key1, bitId: b1, peer: null});
+        willRespondNode.recvOffer = function (offer, recvAnswerCallback) {
+          var retKey = this.id;
+          kademlia.mockTime.setTimeout(function () {
+            recvAnswerCallback(retKey);
+          }, 10);
+        };
+        dht._insertNode(willRespondNode);
+      }
+      console.log(dht);
+
+      var retVal = null;
+
+      var callbackFn = function (answers) {
+        retVal = answers;
+      };
+
+      dht.recvFindNodePrimitive('08000001', '00000000', keys, callbackFn);
+
+      kademlia.mockTime.advance(20);
+
+      var retKeys = [
+        '08000001',
+        '10000001',
+        '20000001',
+        '40000001',
+        '80000001',
+      ];
+      assert.deepEqual(retVal.sort(), retKeys);
     });
   });
 });
