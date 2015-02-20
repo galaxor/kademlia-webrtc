03ee4ba6bb9fca0c27440e450040c351d8208179
Ryan Hughes <ryan@iheartryan.com>
2015-02-20 08:42:22 -0500

    Test of returning when everybody responds.

--- kademlia/test/run.js
+++ kademlia/test/run.js
@@ -195,7 +195,31 @@ describe("KademliaDHT", function () {
     });
 
     it("should return a partially-full bucket if there are not enough nodes to fill a bucket, and should return as soon as all nodes respond.", function () {
-      assert(0);
+      var kademlia = mockTimedKademlia();
+
+      var dht = new kademlia.KademliaDHT({B: 32, id: '00000000'});
+
+      var key1 = '80000001';
+      var b1   = dht._hex2BitStream(key1);
+      var willRespondNode = new kademlia.KademliaRemoteNode({id: key1, bitId: b1, peer: null});
+      willRespondNode.recvOffer = function (offer, recvAnswerCallback) {
+        kademlia.mockTime.setTimeout(function () {
+          recvAnswerCallback('good call');
+        }, 10);
+      };
+      dht._insertNode(willRespondNode);
+
+      var retVal = null;
+
+      var callbackFn = function (answers) {
+        retVal = answers;
+      };
+
+      dht.recvFindNodePrimitive('80000001', '00000000', ['fake offer', 'flake offer'], callbackFn);
+
+      kademlia.mockTime.advance(20);
+
+      assert.deepEqual(retVal, ['good call']);
     });
 
     it("should return a full bucket if there are enough nodes to fill a bucket.", function () {
